16c9c42c7c8046067e7142d3a1979429af9928aa
Andrii Herheliuk <andrii@herheliuk.com>
2025-10-18 17:21:09 +0100

    pushed

---
+++ test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+x = 5
+y = 10
+
+#from one import hey
+
+#hey()
+
+def adD(a, b): #comm
+    tqeww = 1
+    del tqeww
+    return a + b
+
+z = adD(x, y)
+f"z = {z}" #comm
+
+for i in [1, 2]:
+    z += i
+
+hey = {'1': 1, 2: f'{2}'}
+
+print(z)
